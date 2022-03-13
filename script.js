17cbccbe66976f97397cf31d95dab4a62647fcc0
b9benign <trairotte@gmail.com>
2022-03-13 17:56:11 +0100

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello!')
+async function renderData() {
+  //get the div
+  //get data
+  //render data
+  var application = document.querySelector('#app');
+  console.log(application);
+  var data = await getData();
+  application.innerHTML = `
+  <div>Name: ${data.name}</div>
+  <div>ID: ${data.id}</div>
+  <div>Height: ${data.height}</div>
+  `;
+}
+async function getData() {
+  var result = await fetch('https://pokeapi.co/api/v2/pokemon/ditto'); // get the data
+
+  var data = await result.json(); // we know this data is JSON, so we can use the JSON function to get the json in javascript
+  return data;
+}
+
+renderData();
